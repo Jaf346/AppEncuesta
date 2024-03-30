e48b5b33a4433553dd5e34d1e899ed3ed9c37734
Usuario <Usuario@DESKTOP-OHI80SJ>
2024-03-30 15:16:47 -0600

    Agregar archivos de proyecto.

---
+++ AppEncuesta/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace AppEncuesta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtIdEncuesta = new System.Windows.Forms.TextBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtapellido = new System.Windows.Forms.TextBox();
+            this.txtfecha = new System.Windows.Forms.TextBox();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.txtcorreo = new System.Windows.Forms.TextBox();
+            this.txtcarro = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idEncuestaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellidoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fechaNacimientoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.edadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.correoElectronicoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.carroPropioDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.encuestasBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.encuestaDataSet = new AppEncuesta.EncuestaDataSet();
+            this.encuestasTableAdapter = new AppEncuesta.EncuestaDataSetTableAdapters.EncuestasTableAdapter();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.encuestasBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.encuestaDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtIdEncuesta
+            // 
+            this.txtIdEncuesta.Location = new System.Drawing.Point(667, 85);
+            this.txtIdEncuesta.Name = "txtIdEncuesta";
+            this.txtIdEncuesta.Size = new System.Drawing.Size(100, 26);
+            this.txtIdEncuesta.TabIndex = 0;
+            this.txtIdEncuesta.TextChanged += new System.EventHandler(this.txtEncuesta_TextChanged);
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(132, 85);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(100, 26);
+            this.txtnombre.TabIndex = 1;
+            // 
+            // txtapellido
+            // 
+            this.txtapellido.Location = new System.Drawing.Point(132, 140);
+            this.txtapellido.Name = "txtapellido";
+            this.txtapellido.Size = new System.Drawing.Size(100, 26);
+            this.txtapellido.TabIndex = 2;
+            // 
+            // txtfecha
+            // 
+            this.txtfecha.Location = new System.Drawing.Point(132, 207);
+            this.txtfecha.Name = "txtfecha";
+            this.txtfecha.Size = new System.Drawing.Size(100, 26);
+            this.txtfecha.TabIndex = 3;
+            // 
+            // txtedad
+            // 
+            this.txtedad.Location = new System.Drawing.Point(132, 275);
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(100, 26);
+            this.txtedad.TabIndex = 4;
+            // 
+            // txtcorreo
+            // 
+            this.txtcorreo.Location = new System.Drawing.Point(132, 348);
+            this.txtcorreo.Name = "txtcorreo";
+            this.txtcorreo.Size = new System.Drawing.Size(100, 26);
+            this.txtcorreo.TabIndex = 5;
+            // 
+            // txtcarro
+            // 
+            this.txtcarro.Location = new System.Drawing.Point(132, 422);
+            this.txtcarro.Name = "txtcarro";
+            this.txtcarro.Size = new System.Drawing.Size(100, 26);
+            this.txtcarro.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 479);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 42);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "INGRESAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(528, 44);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(118, 43);
+            this.btnModificar.TabIndex = 9;
+            this.btnModificar.Text = "MODIFICAR";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(786, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 42);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "ELIMINAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idEncuestaDataGridViewTextBoxColumn,
+            this.nombreDataGridViewTextBoxColumn,
+            this.apellidoDataGridViewTextBoxColumn,
+            this.fechaNacimientoDataGridViewTextBoxColumn,
+            this.edadDataGridViewTextBoxColumn,
+            this.correoElectronicoDataGridViewTextBoxColumn,
+            this.carroPropioDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.encuestasBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(383, 140);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1526, 324);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // idEncuestaDataGridViewTextBoxColumn
+            // 
+            this.idEncuestaDataGridViewTextBoxColumn.DataPropertyName = "IdEncuesta";
+            this.idEncuestaDataGridViewTextBoxColumn.HeaderText = "IdEncuesta";
+            this.idEncuestaDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.idEncuestaDataGridViewTextBoxColumn.Name = "idEncuestaDataGridViewTextBoxColumn";
+            this.idEncuestaDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idEncuestaDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // nombreDataGridViewTextBoxColumn
+            // 
+            this.nombreDataGridViewTextBoxColumn.DataPropertyName = "Nombre";
+            this.nombreDataGridViewTextBoxColumn.HeaderText = "Nombre";
+            this.nombreDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.nombreDataGridViewTextBoxColumn.Name = "nombreDataGridViewTextBoxColumn";
+            this.nombreDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // apellidoDataGridViewTextBoxColumn
+            // 
+            this.apellidoDataGridViewTextBoxColumn.DataPropertyName = "Apellido";
+            this.apellidoDataGridViewTextBoxColumn.HeaderText = "Apellido";
+            this.apellidoDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.apellidoDataGridViewTextBoxColumn.Name = "apellidoDataGridViewTextBoxColumn";
+            this.apellidoDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // fechaNacimientoDataGridViewTextBoxColumn
+            // 
+            this.fechaNacimientoDataGridViewTextBoxColumn.DataPropertyName = "FechaNacimiento";
+            this.fechaNacimientoDataGridViewTextBoxColumn.HeaderText = "FechaNacimiento";
+            this.fechaNacimientoDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.fechaNacimientoDataGridViewTextBoxColumn.Name = "fechaNacimientoDataGridViewTextBoxColumn";
+            this.fechaNacimientoDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // edadDataGridViewTextBoxColumn
+            // 
+            this.edadDataGridViewTextBoxColumn.DataPropertyName = "Edad";
+            this.edadDataGridViewTextBoxColumn.HeaderText = "Edad";
+            this.edadDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.edadDataGridViewTextBoxColumn.Name = "edadDataGridViewTextBoxColumn";
+            this.edadDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // correoElectronicoDataGridViewTextBoxColumn
+            // 
+            this.correoElectronicoDataGridViewTextBoxColumn.DataPropertyName = "CorreoElectronico";
+            this.correoElectronicoDataGridViewTextBoxColumn.HeaderText = "CorreoElectronico";
+            this.correoElectronicoDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.correoElectronicoDataGridViewTextBoxColumn.Name = "correoElectronicoDataGridViewTextBoxColumn";
+            this.correoElectronicoDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // carroPropioDataGridViewTextBoxColumn
+            // 
+            this.carroPropioDataGridViewTextBoxColumn.DataPropertyName = "CarroPropio";
+            this.carroPropioDataGridViewTextBoxColumn.HeaderText = "CarroPropio";
+            this.carroPropioDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.carroPropioDataGridViewTextBoxColumn.Name = "carroPropioDataGridViewTextBoxColumn";
+            this.carroPropioDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // encuestasBindingSource
+            // 
+            this.encuestasBindingSource.DataMember = "Encuestas";
+            this.encuestasBindingSource.DataSource = this.encuestaDataSet;
+            // 
+            // encuestaDataSet
+            // 
+            this.encuestaDataSet.DataSetName = "EncuestaDataSet";
+            this.encuestaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // encuestasTableAdapter
+            // 
+            this.encuestasTableAdapter.ClearBeforeFill = true;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(1349, 488);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(118, 42);
+            this.btnActualizar.TabIndex = 12;
+            this.btnActualizar.Text = "ACTUALIZAR";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 388);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "TIENES CARRO?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "NOMBRE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "APELLIDO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(663, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "#ENCUESTA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(301, 20);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "CUAL ES TU FECHA DE NACIMIENTO?";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(104, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(162, 20);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "CUAL ES TU EDAD?";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(93, 325);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(186, 20);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "CUAL ES TU CORREO?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1493, 723);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtcarro);
+            this.Controls.Add(this.txtcorreo);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.txtfecha);
+            this.Controls.Add(this.txtapellido);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.txtIdEncuesta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.encuestasBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.encuestaDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIdEncuesta;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtapellido;
+        private System.Windows.Forms.TextBox txtfecha;
+        private System.Windows.Forms.TextBox txtedad;
+        private System.Windows.Forms.TextBox txtcorreo;
+        private System.Windows.Forms.TextBox txtcarro;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private EncuestaDataSet encuestaDataSet;
+        private System.Windows.Forms.BindingSource encuestasBindingSource;
+        private EncuestaDataSetTableAdapters.EncuestasTableAdapter encuestasTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idEncuestaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombreDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellidoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fechaNacimientoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn edadDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn correoElectronicoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn carroPropioDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ AppEncuesta/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppEncuesta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string conexion = "Data Source=DESKTOP-OHI80SJ\\SQLEXPRESS;Initial Catalog=Encuesta;Integrated Security=True;";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO Encuestas (Nombre, Apellido, FechaNacimiento, Edad, CorreoElectronico, CarroPropio) values (@Nombre, @Apellido, @FechaNacimiento, @Edad, @CorreoElectronico, @CarroPropio)", cn);
+
+                cmd.Parameters.AddWithValue("@Nombre", txtnombre.Text);
+                cmd.Parameters.AddWithValue("@Apellido", txtapellido.Text);
+
+                
+                DateTime fechaNacimiento;
+                if (DateTime.TryParse(txtfecha.Text, out fechaNacimiento))
+                {
+                    cmd.Parameters.AddWithValue("@FechaNacimiento", fechaNacimiento);
+                }
+                else
+                {
+                    
+                    fechaNacimiento = DateTime.MinValue;
+                    cmd.Parameters.AddWithValue("@FechaNacimiento", fechaNacimiento);
+                }
+
+                int edad;
+                if (int.TryParse(txtedad.Text, out edad))
+                {
+                    cmd.Parameters.AddWithValue("@Edad", edad);
+                }
+                else
+                {
+                    
+                    cmd.Parameters.AddWithValue("@Edad", DBNull.Value);
+                }
+
+                cmd.Parameters.AddWithValue("@CorreoElectronico", txtcorreo.Text);
+                cmd.Parameters.AddWithValue("@CarroPropio", txtcarro.Text);
+
+                cmd.CommandType = CommandType.Text;
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.encuestasTableAdapter.Fill(this.encuestaDataSet.Encuestas);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(conexion))
+                {
+                    SqlCommand cmd = new SqlCommand("UPDATE Encuestas SET Nombre = @Nombre, Apellido = @Apellido, FechaNacimiento = @FechaNacimiento, Edad = @Edad, CorreoElectronico = @CorreoElectronico, CarroPropio = @CarroPropio WHERE IdEncuesta = @IdEncuesta", cn);
+
+                    cmd.Parameters.AddWithValue("@Nombre", txtnombre.Text);
+                    cmd.Parameters.AddWithValue("@Apellido", txtapellido.Text);
+
+                    
+                    DateTime fechaNacimiento;
+                    if (DateTime.TryParse(txtfecha.Text, out fechaNacimiento))
+                    {
+                        cmd.Parameters.AddWithValue("@FechaNacimiento", fechaNacimiento);
+                    }
+                    else
+                    {
+                        fechaNacimiento = DateTime.MinValue;
+                        cmd.Parameters.AddWithValue("@FechaNacimiento", fechaNacimiento);
+                    }
+
+                    int edad;
+                    if (int.TryParse(txtedad.Text, out edad))
+                    {
+                        cmd.Parameters.AddWithValue("@Edad", edad);
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@Edad", DBNull.Value);
+                    }
+
+                    cmd.Parameters.AddWithValue("@CorreoElectronico", txtcorreo.Text);
+                    cmd.Parameters.AddWithValue("@CarroPropio", txtcarro.Text);
+                    cmd.Parameters.AddWithValue("@IdEncuesta", txtIdEncuesta.Text); 
+
+                    cmd.CommandType = CommandType.Text;
+
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void txtEncuesta_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(conexion))
+                {
+                    SqlCommand cmd = new SqlCommand("DELETE FROM Encuestas WHERE IdEncuesta = @IdEncuesta", cn);
+
+                    
+                    if (!string.IsNullOrEmpty(txtIdEncuesta.Text))
+                    {
+                        cmd.Parameters.AddWithValue("@IdEncuesta", txtIdEncuesta.Text);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Por favor, ingrese un IdEncuesta válido.");
+                        return;
+                    }
+
+                    cmd.CommandType = CommandType.Text;
+
+                    cn.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Registro eliminado exitosamente.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se encontró ningún registro con el IdEncuesta proporcionado.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                this.encuestasTableAdapter.Fill(this.encuestaDataSet.Encuestas);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar el DataGridView: " + ex.Message);
+            }
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
